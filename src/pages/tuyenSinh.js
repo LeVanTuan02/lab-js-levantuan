251f371edf30dd2f992b5a0ca971eabb812d4e84
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-01-12 20:32:40 +0700

    style: update css

--- src/pages/tuyenSinh.js
+++ src/pages/tuyenSinh.js
@@ -1,7 +1,7 @@
 const TuyenSinhPage = {
     render() {
         return `
-        <section class="mt-3 h-[75vh] relative bg-no-repeat bg-cover bg-center" style="background-image: url(https://caodang.fpt.edu.vn/wp-content/uploads/3dfgd.jpg);">
+        <section class="h-[calc(100vh-200px)] relative bg-no-repeat bg-cover bg-center" style="background-image: url(https://caodang.fpt.edu.vn/wp-content/uploads/3dfgd.jpg);">
             <div class="absolute top-0 right-0 bottom-0 left-0 bg-blue-900 opacity-50"></div>
             <div class="absolute w-full h-full text-center text-white z-10 flex flex-col items-center justify-center px-24">
                 <span class="block font-bold text-5xl leading-tight">40.000+ sinh viên đã lựa chọn Cao đẳng FPT</span>
