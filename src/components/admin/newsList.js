073a46f1521b0c287996a02712374ed4ea431ef2
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-01-12 13:30:42 +0700

    add link title

--- src/components/admin/newsList.js
+++ src/components/admin/newsList.js
@@ -30,7 +30,7 @@ const AdminNewsList = {
                             ${post.id}
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                            ${post.title.substr(0, 50)}...
+                            <a href="/news/${post.id}" class="hover:underline">${post.title.substr(0, 50)}...</a>
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                             ${post.description.substr(0, 50)}...
