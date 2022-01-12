d43f5fd5397a2e3183785ec503fa3fa66d2061ac
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-01-12 20:35:54 +0700

    style: update css

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 const Footer = {
     render() {
         return `
-        <div class="bg-blue-900 text-center mt-5 text-white py-4">
+        <div class="bg-blue-900 text-center text-white py-4">
             <a href="https://www.facebook.com/LeVanTuan.Info/" class="hover:text-red-500 transition">Lê Văn Tuân</a>
             -
             <span>FPT Polytechnic</span>
