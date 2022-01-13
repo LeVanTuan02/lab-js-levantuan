6ceb0ae4ca77d5407c6bd75bb13930ef2a786f2b
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-01-13 23:42:21 +0700

    add search news

--- src/pages/admin/news/index.js
+++ src/pages/admin/news/index.js
@@ -24,6 +24,12 @@ const AdminNewsPage = {
             <main>
                 <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                     <div class="px-4 py-6 sm:px-0">
+                        <div class="flex rounded-md shadow-sm mb-5">
+                            <input type="text" name="company-website" id="company-website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-l-md sm:text-sm border-gray-300 px-4 py-2 border outline-none" placeholder="Nhập tiêu đề bài viết...">
+                            <span class="inline-flex items-center px-3 rounded-r-md border border-l-0 border-gray-300 bg-gray-50 text-gray-500 text-sm cursor-pointer hover:bg-gray-200">
+                                <i class="fas fa-search"></i>
+                            </span>
+                        </div>
                         <div class="flex flex-col">
                             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
