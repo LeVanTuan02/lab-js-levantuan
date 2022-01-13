db04f924ba57d89fb2ef1a0b6260aa9507e42e62
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-01-13 14:16:42 +0700

    add link dashboard

--- src/components/nav.js
+++ src/components/nav.js
@@ -9,6 +9,9 @@ const Nav = {
                     <a href="${menu.url}" class="block p-3 text-white transition hover:underline">${menu.title}</a>
                 </li>
                 `).join("")}
+                <li>
+                    <a href="/admin/dashboard" class="block p-3 text-white transition hover:underline">Admin</a>
+                </li>
         </ul>
         `;
     },
