782ed2b3ea5e2dacdd0f55625cc10d2405b8259a
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-01-14 10:35:11 +0700

    update link nav

--- src/components/admin/nav.js
+++ src/components/admin/nav.js
@@ -31,14 +31,14 @@ const AdminNav = {
                         <div>
                             <button type="button" class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                <img class="h-8 w-8 rounded-full object-cover" src="https://res.cloudinary.com/levantuan/image/upload/v1642130754/fpoly/js/LeVanTuan01_cvmsex.jpg" alt="">
                             </button>
                         </div>
                         <div class="hidden group-hover:block pt-2 origin-top-right absolute right-0">
                             <div class="w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                                 <a href="#" class="hover:bg-gray-100 hover:text-black block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
                                 <a href="#" class="hover:bg-gray-100 hover:text-black block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                                <a href="#" class="hover:bg-gray-100 hover:text-black block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                                <a href="/" class="hover:bg-gray-100 hover:text-black block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
                             </div>
                         </div>
                     </div>
@@ -70,7 +70,7 @@ const AdminNav = {
                 <div class="pt-4 pb-3 border-t border-gray-700">
                     <div class="flex items-center px-5">
                         <div class="flex-shrink-0">
-                            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                            <img class="h-10 w-10 rounded-full object-cover" src="https://res.cloudinary.com/levantuan/image/upload/v1642130754/fpoly/js/LeVanTuan01_cvmsex.jpg" alt="">
                         </div>
                         <div class="ml-3">
                             <div class="text-base font-medium leading-none text-white">Lê Văn Tuân</div>
@@ -86,7 +86,7 @@ const AdminNav = {
                     <div class="mt-3 px-2 space-y-1">
                         <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Your Profile</a>
                         <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Settings</a>
-                        <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Sign out</a>
+                        <a href="/" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Sign out</a>
                     </div>
                 </div>
             </div>
