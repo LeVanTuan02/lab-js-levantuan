41866e95357dda62c9f28bd2b3726f6b2e7c361d
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-01-15 10:36:17 +0700

    style: update css button

--- src/components/admin/newsList.js
+++ src/components/admin/newsList.js
@@ -41,8 +41,8 @@ const AdminNewsList = {
                             `}
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                            <a href="/admin/news/${post.id}/edit" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                            <a href="#" class="text-indigo-600 hover:text-indigo-900 ml-3">Delete</a>
+                            <a href="/admin/news/${post.id}/edit" class="h-8 inline-flex items-center px-3 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Edit</a>
+                            <button class="h-8 inline-flex items-center px-3 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-3">Delete</button>
                         </td>
                     </tr>
                     `).join("")}
